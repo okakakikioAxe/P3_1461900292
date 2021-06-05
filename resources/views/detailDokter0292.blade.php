cbc2281bd1519cbeacfceb78cd9235f0808fe75e
Lukman M <lukmanmuhamadilham@gmail.com>
2021-06-05 11:41:29 +0700

    edit tampilan detailDokter

--- resources/views/detailDokter0292.blade.php
+++ resources/views/detailDokter0292.blade.php
@@ -4,29 +4,34 @@
 
 @section('konten')
     @foreach ($data as $dokter)
+    
     <div class="container">
+        <h2 class="text-center">Detail Dokter</h2>
+        <br>
         <div class="row">
             <div class="col">
                 <h2>Id</h2>
             </div>
             <div class="col">
-                <h2>{{ $dokter->id }}</h2>
+                <h2>: {{ $dokter->id }}</h2>
             </div>  
         </div>
+        <br>
         <div class="row">
             <div class="col">
                 <h2>Nama</h2>
             </div>
             <div class="col">
-                <h2>{{ $dokter->nama }}</h2>
+                <h2>: {{ $dokter->nama }}</h2>
             </div>  
         </div>
+        <br>
         <div class="row">
             <div class="col">
                 <h2>Jabatan</h2>
             </div>
             <div class="col">
-                <h2>{{ $dokter->jabatan }}</h2>
+                <h2>: {{ $dokter->jabatan }}</h2>
             </div>  
         </div>
     </div>
