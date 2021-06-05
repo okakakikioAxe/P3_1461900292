f97384d5cfcebc3dc1a94155ca907671dbb75f91
Lukman M <lukmanmuhamadilham@gmail.com>
2021-06-05 11:01:05 +0700

    edit index PasienController

--- app/Http/Controllers/PasienController.php
+++ app/Http/Controllers/PasienController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Pasien;
 
 class PasienController extends Controller
 {
@@ -13,7 +14,8 @@ class PasienController extends Controller
      */
     public function index()
     {
-        //
+        $data = Pasien::all();
+        return view('dataPasien0292', ['data'=>$data]);
     }
 
     /**
