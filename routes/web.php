d3c4c8faee377b2e2fc34c336b146bb5535f5472
Lukman M <lukmanmuhamadilham@gmail.com>
2021-06-05 10:46:14 +0700

    tambah rute resource kamar

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DokterController;
+use App\Http\Controllers\KamarController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,3 +19,4 @@ Route::get('/', function () {
 });
 
 Route::resource('dokter', DokterController::class);
+Route::resource('kamar', KamarController::class);
