7cda8a45ad1364c569777c31f66525959cec4940
Lukman M <lukmanmuhamadilham@gmail.com>
2021-06-05 11:52:39 +0700

    tambah file editDokter

---
+++ resources/views/editDokter0292.blade.php
@@ -0,0 +1,29 @@
+@extends('template.main0292')
+
+@section('title', 'Edit Dokter')
+
+@section('konten')
+    <div class="container">
+        @foreach ($data as $dokter)
+            <form action="{{ url('dokter/'.$dokter->id) }}">
+                @csrf
+                @method('PUT')
+                <div class="mb-3">
+                    <label for="id" class="form-label">Id</label>
+                    <input type="text" class="form-control" id="id" name="id"  disabled>
+                </div>
+                <div class="mb-3">
+                    <label for="nama" class="form-label">Nama</label>
+                    <input type="text" class="form-control" id="nama" name="nama">
+                </div>
+
+                <div class="mb-3">
+                    <label for="jabatan" class="form-label">Jabatan</label>
+                    <input type="text" class="form-control" id="jabatan" name="jabatan">
+                </div>
+                
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        @endforeach
+    </div>
+@endsection
