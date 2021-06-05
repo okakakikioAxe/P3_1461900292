93df59ab7dba918bd7bd0be52ae853ceb326b375
Lukman M <lukmanmuhamadilham@gmail.com>
2021-06-05 15:38:07 +0700

    edit fungsi destroy

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -131,6 +131,7 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        User::where('id',$id)->delete();
+        return redirect()->route('user.index');
     }
 }
