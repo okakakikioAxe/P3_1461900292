ea1b4423a66bbd40e174a0841db775beedcdf75d
Lukman M <lukmanmuhamadilham@gmail.com>
2021-06-05 11:24:24 +0700

    tambah tombol option

--- resources/views/dataDokter0292.blade.php
+++ resources/views/dataDokter0292.blade.php
@@ -3,13 +3,14 @@
 @section('title', 'Data Dokter')
 
 @section('tabel')
-    <div class="container">
+    <div class="container text-center">
     <table class="table">
         <thead>
             <tr>
-                <th scope="col">id</th>
-                <th scope="col">nama</th>
-                <th scope="col">jabatan</th>
+                <th scope="col">Id</th>
+                <th scope="col">Nama</th>
+                <th scope="col">Jabatan</th>
+                <th scope="col">Option</th>
             </tr>
         </thead>
         <tbody>
@@ -18,6 +19,15 @@
                 <td>{{ $dokter->id }}</td>
                 <td>{{ $dokter->nama }}</td>
                 <td>{{ $dokter->jabatan }}</td>
+                <td>
+                <a class="btn btn-info " href="#" role="button">Detail</a>
+                <a class="btn btn-warning " href="#" role="button">Edit</a>
+                <a class="btn btn-danger " href="#" role="button">Delete</a>
+                </td>
+
+                
+
+                
             </tr>
         @endforeach
         </tbody>
