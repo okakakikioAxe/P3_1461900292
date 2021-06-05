a2b1d6e78125ec9f7ebbbeea4d958f2e49af8d98
Lukman M <lukmanmuhamadilham@gmail.com>
2021-06-05 09:57:28 +0700

    tambah file home

---
+++ resources/views/home0292.blade.php
@@ -0,0 +1,19 @@
+@extends('template.main0292')
+
+@section('title','home')
+
+@section('konten')
+<div class="container">
+    <h1>Selamat datang di</h1>
+    <h1>Sistem Informasi</h1>
+    <h1>Rumah Sakit .</h1>
+</div>
+@endsection
+
+@section('style')
+<style>
+    h1{
+        color : #801ee3;
+    }
+</style>
+@endsection
