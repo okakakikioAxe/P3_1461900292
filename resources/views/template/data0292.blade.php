e3f829f96b66f4237073248d503aace76e89571c
Lukman M <lukmanmuhamadilham@gmail.com>
2021-06-05 12:05:25 +0700

    tambah tombol add dataDokter

--- resources/views/template/data0292.blade.php
+++ resources/views/template/data0292.blade.php
@@ -3,17 +3,24 @@
 @section('konten')
 <div class="container">
     <div class="dropdown">
-    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
-        Pilih Data   
-    </button>
-    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
-        <li><a class="dropdown-item" href="{{ url('dokter') }}">Dokter</a></li>
-        <li><a class="dropdown-item" href="{{ url('pasien') }}">Pasien</a></li>
-        <li><a class="dropdown-item" href="{{ url('kamar') }}">Kamar</a></li>
-        <li><a class="dropdown-item" href="{{ url('user') }}">User</a></li>
-    </ul>
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+            Pilih Data   
+        </button>
+        <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
+            <li><a class="dropdown-item" href="{{ url('dokter') }}">Dokter</a></li>
+            <li><a class="dropdown-item" href="{{ url('pasien') }}">Pasien</a></li>
+            <li><a class="dropdown-item" href="{{ url('kamar') }}">Kamar</a></li>
+            <li><a class="dropdown-item" href="{{ url('user') }}">User</a></li>
+        </ul>
+        </div>
     </div>
+    <div class="row">
+        <div class="col"></div>
+        <div class="col"></div>
+        <div class="col"><a class="btn btn-primary " href="{{ url('dokter/create') }}" role="button">add+</a></div>
+    </div>    
 </div>
+    
 <br>
 @yield('tabel')
 @endsection
