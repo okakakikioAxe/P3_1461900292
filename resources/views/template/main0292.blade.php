26049910df1e6b9135572fc263d894682de31fff
Lukman M <lukmanmuhamadilham@gmail.com>
2021-06-05 09:55:24 +0700

    edit warna link

--- resources/views/template/main0292.blade.php
+++ resources/views/template/main0292.blade.php
@@ -14,13 +14,13 @@
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Home</a>
+                <a class="navbar-brand" href="#" style="color:#181fed;">Home</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <a class="nav-link active" aria-current="page" href="#">Data</a>
+                    <a class="nav-link active" aria-current="page" href="#" style="color:#181fed;">Data</a>
                 </div>
                 </div>
             </div>
